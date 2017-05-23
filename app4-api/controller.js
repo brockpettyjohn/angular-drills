f9227115e383a56ef0826f4d76460940711f9f48
Brock Pettyjohn <brockpettyjohn@gmail.com>
2017-05-22 22:55:31 -0600

    more practice

--- app4-api/controller.js
+++ app4-api/controller.js
@@ -1,4 +1,11 @@
 angular.module('app').controller('mainCtrl', function ($scope, mainSvc){
 $scope.test = mainSvc.test
 
+
+$scope.redFerns = function(){
+    mainSvc.dataFiles().then(function(response){
+        $scope.planets = response.data.results
+    })
+}
+$scope.redFerns()
 })
--- app4-api/service.js
+++ app4-api/service.js
@@ -1,3 +1,7 @@
 angular.module('app').service('mainSvc', function($http){
 this.test = 'unicorn ninja'
+
+    this.dataFiles = function(){
+        return $http.get('http://swapi.co/api/planets')
+    }
 })
